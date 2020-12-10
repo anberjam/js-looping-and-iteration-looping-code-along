89330dc7b0ffaf3912d29097f5eed16629e38888
anberjamz <anberjam@yahoo.com>
2020-12-09 19:06:01 -0500

    commit tree 60e1478bfdb69ed6230cfc56e9d39b7a97b1a8fb

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arrNames, eventName){
+    let cards = []
+    for (let i = 0; i < arrNames.length; i++) {
+        cards.push(`Thank you, ${arrNames[i]}, for the wonderful ${eventName} gift!`)
+        } 
+        return cards   
+}
+
+
+function countDown (posInt) {
+ while (posInt >= 0) {
+     console.log(posInt)
+     posInt--
+ }
+}
